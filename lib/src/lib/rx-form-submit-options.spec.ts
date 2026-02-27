59e2f5e1f53cdef91716d0e024fe50f9890ca513
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-02-27 14:21:41 +0100

    options spec

---
+++ lib/src/lib/rx-form-submit-options.spec.ts
@@ -0,0 +1,72 @@
+import { Component, signal } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { form, submit, type TreeValidationResult } from '@angular/forms/signals';
+import { asyncScheduler, Observable, of, scheduled } from 'rxjs';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { rxFormSubmitOptions } from './rx-form-submit-options';
+
+describe('rxFormSubmitOptions', () => {
+  @Component({
+    template: '',
+  })
+  class TestComponent {
+    observable!: Observable<TreeValidationResult>;
+    form = form(
+      signal({
+        test: '',
+      }),
+      {
+        submission: rxFormSubmitOptions({
+          action: () => this.observable,
+        }),
+      },
+    );
+  }
+  let componentInstance: TestComponent;
+
+  beforeEach(() => {
+    const componentFixture = TestBed.createComponent(TestComponent);
+    componentInstance = componentFixture.componentInstance;
+  });
+
+  it('should succeed when returning undefined', async () => {
+    componentInstance.observable = scheduled(of(undefined), asyncScheduler);
+    await submit(componentInstance.form);
+
+    expect(componentInstance.form().valid()).toBe(true);
+  });
+
+  it('should succeed when returning null', async () => {
+    componentInstance.observable = scheduled(of(null), asyncScheduler);
+    await submit(componentInstance.form);
+
+    expect(componentInstance.form().valid()).toBe(true);
+  });
+
+  it('should be invalid when returning one validation error', async () => {
+    const treeValidationResult: TreeValidationResult = {
+      kind: 'apiError',
+    };
+    componentInstance.observable = scheduled(of(treeValidationResult), asyncScheduler);
+    await submit(componentInstance.form);
+
+    expect(componentInstance.form().valid()).toBe(false);
+    expect(componentInstance.form().errors()[0]).toEqual(treeValidationResult);
+  });
+
+  it('should be invalid when returning multiple validation errors', async () => {
+    const error1: TreeValidationResult = {
+      kind: 'apiError1',
+    };
+    const error2: TreeValidationResult = {
+      kind: 'apiError2',
+    };
+    const treeValidationResult: TreeValidationResult = [error1, error2];
+    componentInstance.observable = scheduled(of(treeValidationResult), asyncScheduler);
+    await submit(componentInstance.form);
+
+    expect(componentInstance.form().valid()).toBe(false);
+    expect(componentInstance.form().errors()[0]).toEqual(error1);
+    expect(componentInstance.form().errors()[1]).toEqual(error2);
+  });
+});
