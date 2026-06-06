c559d23cdcb5b6b87b6471eb846548753a7c8ff9
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-06-06 10:35:47 +0200

    remove override of Angular interface

--- lib/src/lib/rx-form-submit-options.ts
+++ lib/src/lib/rx-form-submit-options.ts
@@ -38,17 +38,4 @@ export interface RxFormSubmitOptions<TRootModel, TSubmittedModel> extends Omit<
    * Using `rxSubmit()` outside an injection context and without providing a `DestroyRef` will throw the `NG0203` runtime error: https://angular.dev/errors/NG0203
    */
   readonly destroyRef?: Readonly<DestroyRef> | undefined;
-
-  // Override Angular options with better signatures for immutability
-  readonly onInvalid?: (
-    field: FieldTree<TRootModel & TSubmittedModel>,
-    detail: {
-      readonly root: FieldTree<TRootModel>;
-      readonly submitted: FieldTree<TSubmittedModel>;
-    },
-  ) => void;
-
-  readonly ignoreValidators?: Required<
-    FormSubmitOptions<TRootModel, TSubmittedModel>
-  >['ignoreValidators'];
 }
