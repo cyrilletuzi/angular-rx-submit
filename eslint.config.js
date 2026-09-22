1ff6755886caad2aab826bfe33b42157576aaa7f
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-09-22 10:35:23 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,7 @@ module.exports = defineConfig([
       'prefer-arrow-callback': 'error',
       'prefer-template': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/method-signature-style': 'error',
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-non-null-assertion': 'error',
       '@typescript-eslint/no-unsafe-argument': 'error',
